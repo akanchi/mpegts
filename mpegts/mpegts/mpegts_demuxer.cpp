3c44fc937409667f316b2aa6acf4ca1da07f9ec0
akanchi <chenpx718@gmail.com>
2018-05-25 18:34:42 +0800

    Reduce redundant code

--- mpegts/mpegts/mpegts_demuxer.cpp
+++ mpegts/mpegts/mpegts_demuxer.cpp
@@ -59,82 +59,46 @@ int MpegTsDemuxer::decode(SimpleBuffer *in, TsFrame *&out)
         }
 
         if (_ts_frames.find(ts_header.pid) != _ts_frames.end()) {
-            uint8_t stream_type = _ts_frames[ts_header.pid]->stream_type;
-            if (stream_type == 0x0f) {
-                if (ts_header.adaptation_field_control == 0x02 || ts_header.adaptation_field_control == 0x03) {
-                    AdaptationFieldHeader adapt_field;
-                    adapt_field.decode(in);
-                    in->skip(adapt_field.adaptation_field_length > 0 ? (adapt_field.adaptation_field_length - 1) : 0);
-                }
+            if (ts_header.adaptation_field_control == 0x02 || ts_header.adaptation_field_control == 0x03) {
+                AdaptationFieldHeader adapt_field;
+                adapt_field.decode(in);
+                in->skip(adapt_field.adaptation_field_length > 0 ? (adapt_field.adaptation_field_length - 1) : 0);
+            }
                 
-                if (ts_header.adaptation_field_control == 0x01 || ts_header.adaptation_field_control == 0x03) {
-                    PESHeader pes_header;
-                    if (ts_header.payload_unit_start_indicator == 0x01) {
-                        if (_ts_frames[ts_header.pid]->completed == true) {
-                            _ts_frames[ts_header.pid]->reset();
-                        }
-
-                        if (!_ts_frames[ts_header.pid]->empty()) {
-                            _ts_frames[ts_header.pid]->completed = true;
-                            _ts_frames[ts_header.pid]->pid = ts_header.pid;
-                            out = _ts_frames[ts_header.pid].get();
-                            in->skip(pos - in->pos());
-                            return stream_type;
-                        }
-
-                        pes_header.decode(in);
-                        _ts_frames[ts_header.pid]->stream_id = pes_header.stream_id;
-                        if (pes_header.pts_dts_flags == 0x02) {
-                            _ts_frames[ts_header.pid]->pts = _ts_frames[ts_header.pid]->dts = read_pts(in);
-                        } else if (pes_header.pts_dts_flags == 0x03) {
-                            _ts_frames[ts_header.pid]->pts = read_pts(in);
-                            _ts_frames[ts_header.pid]->dts = read_pts(in);
-                        }
+            if (ts_header.adaptation_field_control == 0x01 || ts_header.adaptation_field_control == 0x03) {
+                PESHeader pes_header;
+                if (ts_header.payload_unit_start_indicator == 0x01) {
+                    if (_ts_frames[ts_header.pid]->completed == true) {
+                        _ts_frames[ts_header.pid]->reset();
                     }
-                    _ts_frames[ts_header.pid]->_data->append(in->data() + in->pos(), 188 - in->pos() - pos);
-                }
-            } else if (stream_type == 0x1b) {
-                if (ts_header.adaptation_field_control == 0x02 || ts_header.adaptation_field_control == 0x03) {
-                    AdaptationFieldHeader adapt_field;
-                    adapt_field.decode(in);
-                    in->skip(adapt_field.adaptation_field_length > 0 ? (adapt_field.adaptation_field_length - 1) : 0);
-                }
-                
-                if (ts_header.adaptation_field_control == 0x01 || ts_header.adaptation_field_control == 0x03) {
-                    PESHeader pes_header;
-                    if (ts_header.payload_unit_start_indicator == 0x01) {
-                        if (_ts_frames[ts_header.pid]->completed == true) {
-                            _ts_frames[ts_header.pid]->reset();
-                        }
 
-                        if (!_ts_frames[ts_header.pid]->empty()) {
-                            _ts_frames[ts_header.pid]->completed = true;
-                            _ts_frames[ts_header.pid]->pid = ts_header.pid;
-                            out = _ts_frames[ts_header.pid].get();
-                            in->skip(pos - in->pos());
-                            return stream_type;
-                        }
-
-                        pes_header.decode(in);
-                        _ts_frames[ts_header.pid]->stream_id = pes_header.stream_id;
-                        if (pes_header.pts_dts_flags == 0x02) {
-                            _ts_frames[ts_header.pid]->pts = _ts_frames[ts_header.pid]->dts = read_pts(in);
-                        } else if (pes_header.pts_dts_flags == 0x03) {
-                            _ts_frames[ts_header.pid]->pts = read_pts(in);
-                            _ts_frames[ts_header.pid]->dts = read_pts(in);
-                        }
-                        if (pes_header.pes_packet_length != 0x0000) {
-                            if ((pes_header.pes_packet_length - 3 - pes_header.header_data_length) >= 188 || (pes_header.pes_packet_length - 3 - pes_header.header_data_length) < 0) {
-                                _ts_frames[ts_header.pid]->_data->append(in->data() + in->pos(), 188 - in->pos() - pos);
-                            } else {
-                                _ts_frames[ts_header.pid]->_data->append(in->data() + in->pos(), pes_header.pes_packet_length - 3 - pes_header.header_data_length);
-                            }
-                            
-                            break;
-                        }
+                    if (!_ts_frames[ts_header.pid]->empty()) {
+                        _ts_frames[ts_header.pid]->completed = true;
+                        _ts_frames[ts_header.pid]->pid = ts_header.pid;
+                        out = _ts_frames[ts_header.pid].get();
+                        in->skip(pos - in->pos());
+                        return _ts_frames[ts_header.pid]->stream_type;
                     }
-                    _ts_frames[ts_header.pid]->_data->append(in->data() + in->pos(), 188 - in->pos() - pos);
+
+                    pes_header.decode(in);
+                    _ts_frames[ts_header.pid]->stream_id = pes_header.stream_id;
+                    if (pes_header.pts_dts_flags == 0x02) {
+                        _ts_frames[ts_header.pid]->pts = _ts_frames[ts_header.pid]->dts = read_pts(in);
+                    } else if (pes_header.pts_dts_flags == 0x03) {
+                        _ts_frames[ts_header.pid]->pts = read_pts(in);
+                        _ts_frames[ts_header.pid]->dts = read_pts(in);
+                    }
+                    if (pes_header.pes_packet_length != 0x0000) {
+                        if ((pes_header.pes_packet_length - 3 - pes_header.header_data_length) >= 188 || (pes_header.pes_packet_length - 3 - pes_header.header_data_length) < 0) {
+                            _ts_frames[ts_header.pid]->_data->append(in->data() + in->pos(), 188 - in->pos() - pos);
+                        } else {
+                            _ts_frames[ts_header.pid]->_data->append(in->data() + in->pos(), pes_header.pes_packet_length - 3 - pes_header.header_data_length);
+                        }
+                        
+                        break;
+                    }
                 }
+                _ts_frames[ts_header.pid]->_data->append(in->data() + in->pos(), 188 - in->pos() - pos);
             }
         }
 
